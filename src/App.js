7e3e4ce65ecbf6fc4b086faecbb6d74a58f9bdc8
Jesus Silva <sistemaseltigre@gmail.com>
2023-04-08 18:42:36 -0400

    creating a tx example to phantomevm

--- src/App.js
+++ src/App.js
@@ -4,27 +4,43 @@ import { ethers } from 'ethers';
 import { Web3Provider } from '@ethersproject/providers';
 
 function App() {
-  // URL of your custom RPC endpoint
-  const customRpcEndpoint = 'https://red-multi-valley.matic-testnet.discover.quiknode.pro/61b21728fa928158390362bfe247eab7ee8c68e7/';
+  async function testingphantom () {
+    const provider = window.ethereum;// see "Detecting the Provider"
+   
+    const quicknodeRPCConfig = {
+      chainId: '0x13881',
+            chainName: 'Polygon',
+            blockExplorerUrls: ['https://polygonscan.com'],
+            nativeCurrency: {symbol: 'MATIC', decimals: 18},
+            rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+    };
 
-  // Create a Web3Provider instance using the custom RPC endpoint
-  const provider = new Web3Provider(new ethers.JsonRpcProvider(customRpcEndpoint));
-
-  async function testphantom() {
-    // Use the provider to send transactions
-    const accounts = await provider.listAccounts();
-    const result = await provider.send('eth_sendTransaction', [
-    {
-      from: accounts[0],
-      to: '0x0c54FcCd2e384b4BB6f2E405Bf5Cbc15a017AaFb',
-      value: '0x0',
-      gasLimit: '0x5028',
-      gasPrice: '0x2540be400',
-      type: '0x0',
-    },
-    ]);
+    try {
+      const accounts = await provider.request({ method: "eth_requestAccounts", params:[quicknodeRPCConfig] });
+      console.log(accounts[0]);
+      testingtx(accounts[0]);
+    } catch (err) {
+      console.log(err)
+    }
   }
-  testphantom();
+  async function testingtx (account) {
+    const result = await provider.request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: account,
+          to: '0x0000000000000000000000000000000000000000',
+          value: '0x0',
+          gasLimit: '0x5028',
+          gasPrice: '0x2540be400',
+          type: '0x0',
+        },
+      ],
+    });
+
+    console.log(result);
+  }
+  testingphantom();
 
   return (
     
