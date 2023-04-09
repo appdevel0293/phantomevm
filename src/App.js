89f36c321a86bcd22cd8b133480032e001563f11
Jesus Silva <sistemaseltigre@gmail.com>
2023-04-09 19:51:14 -0400

    creating a ne code to send tx in polygon mumbai using phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,69 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import { ethers } from 'ethers';
-import { Web3Provider } from '@ethersproject/providers';
 
 function App() {
- /*  console.log("detectamos el provider");
-  const provider = window.ethereum;// see "Detecting the Provider"
-  console.log("provider:",provider);
-  
-  const quicknodeRPCConfig = {
-    chainId: '0x13881',
-    chainName: 'Polygon',
-    blockExplorerUrls: ['https://polygonscan.com'],
-    nativeCurrency: {symbol: 'MATIC', decimals: 18},
-    rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
-  };
-  
-  async function testingphantom () {
-    console.log("testeamos conectar con el provider");
-    try {
-      const accounts = await provider.request({ method: "eth_requestAccounts", params:[quicknodeRPCConfig] });
-      console.log("cuenta obtenida del provider",accounts[0]);
-      testingtx(accounts[0],0);
-    } catch (err) {
-      console.log(err)
-    }
-  }
 
-  async function changechain (account) {
-    console.log("cambiamos a testnet");
-    try {
-      const changechainid = await provider.request({ method: "wallet_addEthereumChain", params:[quicknodeRPCConfig] });
-      testingtx(account,changechainid);
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  //create var to save account
+  var account_client = null;
 
-  async function testingtx (account,changechainid) {
-    console.log("cuenta=>",account);
-    console.log("chaindata=>",changechainid);
-    const result = await provider.request({
-      method: 'eth_sendTransaction',
-      params: [
-        {
-          from: account,
-          to: '0x0000000000000000000000000000000000000000',
-          value: '0x0',
-          gasLimit: '0x5028',
-          gasPrice: '0x2540be400',
-          type: '0x0',
-          chainId: '0x13881',
-          chainName: 'Polygon',
-          blockExplorerUrls: ['https://polygonscan.com'],
-          nativeCurrency: {symbol: 'MATIC', decimals: 18},
-          rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
-        },
-      ],
-    });
-
-    console.log(result);
-  }
-  
-  testingphantom(); */
-
+  //create const to set provider, chain and RPC
   const quicknodeRPCConfig = {
     chainId: '0x13881',
     chainName: 'Polygon',
@@ -71,68 +13,80 @@ function App() {
     nativeCurrency: {symbol: 'MATIC', decimals: 18},
     rpcUrls: ['https://red-multi-valley.matic-testnet.discover.quiknode.pro/61b21728fa928158390362bfe247eab7ee8c68e7/'],
   };
+
+  //Verify if phantom exist
   const isPhantomInstalled = window?.phantom?.ethereum?.isPhantom;
   console.log(isPhantomInstalled);
+
+
+  //async function to get provider and change chain and RPC
   const getProvider = async () => {
+    
+    //if phantom wallet exist
     if (isPhantomInstalled) {
-      console.log("inside if");
+      
       const anyWindow = window;
-      console.log("window",window);
       const provider = anyWindow.phantom?.ethereum;
-      console.log("provider",provider);
      
       if (provider) {
-        
-        /* provider.request({
-          method: 'wallet_switchEthereumChain',
-          params: [quicknodeRPCConfig]
-        }).then(() => {
-          console.log("changeprovider=>",provider)
-          return provider;
-        }).catch((error) => {
-          console.log(error)
-        }); */
-         // Request permission to access the wallet_switchEthereumChain method
          provider.request({
           method: 'eth_requestAccounts'
         }).then((accounts) => {
-          // Permission granted, switch the network
+          
+          //get account to Mint and Send NFT
+          account_client = accounts[0];
+
+          // Permission granted, switch the network to Polygon Mumbai
           provider.request({
             method: 'wallet_switchEthereumChain',
             params: [quicknodeRPCConfig]
           }).then(() => {
-            // The user has switched to the Polygon network
-            // Now you can send transactions on the Polygon network
+            
+            //send log to verify account and provider
+            console.log(account_client);
+            console.log(provider);
+
           }).catch((error) => {
             console.error(error);
           });
+
         }).catch((error) => {
           console.error(error);
         });
-        
-        
-        //return provider;
       }
     }else{
+
+      //if phantom wallet not installed, redirect to official website
       window.open('https://phantom.app/', '_blank');
+    
     }  
-   
   };
-  // getProvider();
-  const provider = getProvider(); // see "Detecting the Provider"
+  
+  //Get provider with Polygon and Mumbai - QuickNode RPC
+  const provider = getProvider();
   
   
-  async function sendtx(){
-    try {
-      const accounts = await provider.request({ method: "eth_requestAccounts" });
-      console.log(accounts[0]);
-    } catch (err) {
-      console.log(err)
-    }
+  //Create a Tx example in Polygon Mumbai network - QuickNode RPC
+  async function testingtx () {
+    console.log("send tx",account_client);
+    const result = await provider.request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: account,
+          to: '0x75e01f1Ebd58302B5b67e67825fa6917749b5896',
+          value: '0x0',
+          gasLimit: '0x5028',
+          gasPrice: '0x2540be400',
+          type: '0x0',
+        },
+      ],
+    });
+
+    //log to see tx result
+    console.log("result_tx=>",result);
   }
-  
-  //sendtx();
-  
+
   return (
     
     <div className="App">
@@ -140,6 +94,7 @@ function App() {
         <p>
           Mint and Verify NFT
         </p>
+        <button onClick={testingtx} >Send TX </button>
       </header>
     </div>
   );
